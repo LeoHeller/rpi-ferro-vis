8042ea9ad0f705b5383977587132741f23057b91
Ledo0071 <Ledo0071@gmail.com>
2016-02-07 10:19:11 +0000

    now with magnets

--- driver/gpio.py
+++ driver/gpio.py
@@ -2,6 +2,9 @@
 import wiringpi2 as w
 from time import sleep
 
+pins_pwm=[4,17,21,22]
+pins_pol=[18,23,24,25]
+
 def all_off():
     for pin in range(1, 26):
         w.digitalWrite(pin, 0)
@@ -9,31 +12,47 @@ def all_off():
 # print raspberry pi board revision
 print(w.piBoardRev())
 
-# use physical pin numbering
-w.wiringPiSetupPhys()
+def main():
+    setup()
+    try:
+        helligkeit = 10
+        while True:
+            magnet (2,helligkeit,0) 
+            sleep(0.01)
+            helligkeit += 1
+            if helligkeit > 100:
+                helligkeit = 0
+             
 
-# set PIN13 to output
-w.pinMode(13, 1)
-# turn PIN13 off
-w.digitalWrite(13,0)
+    finally:
+         # turn all pins off
+        all_off()
 
-w.pinMode(19, 1)
-w.digitalWrite(19,1)
+def setup():
+    # use Gpio pin numbering
+    w.wiringPiSetupGpio()
 
-w.softPwmCreate(13,0,100)
-w.softPwmCreate(19,0,100)
+    # polarity pins are outputs:
+    for pin in pins_pol:
+        w.pinMode(pin, 1)
+        w.digitalWrite(pin,0)
 
-try:
-    helligkeit = 0
-    while True:
-        w.softPwmWrite(19,100-helligkeit) 
-        w.softPwmWrite(13,helligkeit)
-        sleep(0.01)
-        helligkeit += 1
-        if helligkeit > 100:
-            helligkeit = 0
-         
+    # PWM pins:
+    for pin in pins_pwm:
+        w.softPwmCreate(pin,0,100)
 
-finally:
-    # turn all pins off
-    all_off()
+def magnet(nummer, staerke, polaritaet):
+    if not ((0 <= nummer <=3) and (0<= staerke <= 100) and (0<= polaritaet <=1)):
+        #fehler        
+        return -1
+    w.softPwmWrite(pins_pwm[nummer],staerke)
+    w.digitalWrite(pins_pol[nummer],polaritaet)
+    return 0
+
+if __name__ == "__main__":
+    main()
+    
+
+
+
+
